39c83cd25e87820874062585f44dd6587e1cc0d3
Brad Russell <brad.russell@e-gineering.com>
2022-10-06 15:09:27 -0400

    Make icon size configurable

--- Egineering.UrlShortener.Services/QRCodeService.cs
+++ Egineering.UrlShortener.Services/QRCodeService.cs
@@ -26,16 +26,19 @@ public class QRCodeService : IQRCodeService
         var canvas = surface.Canvas;
 
         var imagePath = Configuration["QRCodeTheme:ImagePath"];
-        var logo = File.ReadAllBytes("wwwroot/images/EG.png");
-        var icon = new IconData
-        {
-            Icon = SKBitmap.Decode(logo),
-            IconSizePercent = 15,
-        };
-
+        _ = int.TryParse(Configuration["QRCodeTheme:IconSizePercent"], out int iconSizePercent);
         _ = float.TryParse(Configuration["QRCodeTheme:Color:Hue"], out float hue);
         _ = float.TryParse(Configuration["QRCodeTheme:Color:Saturation"], out float saturation);
         _ = float.TryParse(Configuration["QRCodeTheme:Color:Lightness"], out float lightness);
+
+        var logo = File.ReadAllBytes(imagePath);
+        var icon = new IconData
+        {
+            Icon = SKBitmap.Decode(logo),
+            // Percentage of the QR code that is covered by the logo.
+            // If this is too big, the QR code might not be able to be read.
+            IconSizePercent = iconSizePercent,
+        };
         canvas.Render(qr, info.Width, info.Height, SKColor.Empty, SKColor.FromHsl(hue, saturation, lightness), icon);
 
         using var image = surface.Snapshot();
